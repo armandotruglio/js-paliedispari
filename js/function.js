9771a2db877dca376a60d3af1700a1d84af63c3c
armandotruglio <armando.truglio1@gmail.com>
2024-08-22 16:19:56 +0200

    Finished palindrome part

--- js/function.js
+++ js/function.js
@@ -1,1 +1,11 @@
-console.log('JS OK');
+/**
+ * Checks if a word is a palindrome
+ * @param {string} word The word to check
+ * @returns {boolean} Returns true if the word is a palindrome, false otherwise 
+ */
+
+function isPalindrome(word) {
+    const invertedWord = word.split('').reverse().join('');
+
+    return word.toLowerCase() === invertedWord.toLowerCase()? true : false;
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('JS OK');
+//PALINDROMA
+
+/*
+Acquisisco la parola
+Inverto la parola
+La confronto con la parola originariamente inserita
+Se si equivalgono allora sono palindrome
+*/
+
+let message = 'La parola inserita NON è palindroma';
+
+//Acquisisco la parola
+const word = prompt('Inserire un parola: ').trim();
+
+//Invoco la funzione per verificare che la parola sia palindroma
+if(isPalindrome(word)) message = 'La parola inserita è palindroma';
+
+console.log(message);
